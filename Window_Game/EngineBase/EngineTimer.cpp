8b005805af14e7f67e6510a237b8255ccd070600
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-10-29 00:00:02 +0900

    타이머 정리

--- Window_Game/EngineBase/EngineTimer.cpp
+++ Window_Game/EngineBase/EngineTimer.cpp
@@ -1,7 +1,7 @@
 #include "PreCompile.h"
 #include "EngineTimer.h"
 
-UEngineTimer::UEngineTimer()
+UEngineTimer::UEngineTimer() :Count{}, PrevTime{}, CurTime{}, TimeCounter {}, dDeltaTime{}, DeltaTime{}
 {
 	//	타이머 성능 Init
 	QueryPerformanceFrequency(&Count);					//	1초에 셀 수 있는 카운트를 Count변수에 저장
@@ -18,26 +18,27 @@ UEngineTimer::~UEngineTimer()
 
 void UEngineTimer::TimeCheck()	// 현재 시간과 이전에 기록된 시간 간의 시간 차이를 계산
 {
-	QueryPerformanceCounter(&CurTime);	// 현재 성능 카운터 값을 기록
+	QueryPerformanceCounter(&CurTime);
 
 	// 경과된 틱을 계산하고 초 단위로 변환
-	double Tick = static_cast<double>(CurTime.QuadPart - PrevTime.QuadPart);	
+	double Tick = static_cast<double>(CurTime.QuadPart - PrevTime.QuadPart);
 	dDeltaTime = Tick / TimeCounter;
 	DeltaTime = static_cast<float>(dDeltaTime);
+
 	// 다음 시간 계산을 위해 PrevTime을 업데이트
 	PrevTime.QuadPart = CurTime.QuadPart;
 }
-void UEngineTimer::TimeStart()	
+void UEngineTimer::TimeStart()
 {
 	QueryPerformanceCounter(&PrevTime);
 }
 
-float UEngineTimer::End()	
+float UEngineTimer::End()
 {
 	TimeCheck();
 	return GetDeltaTime();
 }
-double UEngineTimer::DEnd()	
+double UEngineTimer::DEnd()
 {
 	TimeCheck();
 	return GetDoubleDeltaTime();
--- Window_Game/EngineBase/EngineTimer.h
+++ Window_Game/EngineBase/EngineTimer.h
@@ -1,42 +1,41 @@
 #pragma once
-#include <Windows.h>
 
-// 설명 :
+// 설명 :타이머클래스
 class UEngineTimer
 {
 public:
-	// constrcuter destructer
+	//	constrcuter destructer
 	UEngineTimer();
 	~UEngineTimer();
 
-	// delete Function
+	//	delete Function
 	UEngineTimer(const UEngineTimer& _Other) = delete;
 	UEngineTimer(UEngineTimer&& _Other) noexcept = delete;
 	UEngineTimer& operator=(const UEngineTimer& _Other) = delete;
 	UEngineTimer& operator=(UEngineTimer&& _Other) noexcept = delete;
 
-	float GetDeltaTime() { return DeltaTime; }
-	double GetDoubleDeltaTime() { return dDeltaTime; }
+	//	inline
+	inline float GetDeltaTime() const { return DeltaTime; }
+	inline double GetDoubleDeltaTime() const { return dDeltaTime; }
 
-	void TimeCheck();
-	void TimeStart();	//	타이머 시작
-	double DEnd();		//	타이머 끝
-	float End();		//	타이머 끝
-
+	//	일반
+	void TimeCheck();		//	DeltaTime 구하는 함수
+	void TimeStart();		//	타이머 시작
+	float End();			//	타이머 끝 (float반환)	
+	double DEnd();			//	타이머 끝 (double반환)
 
 private:
-	LARGE_INTEGER Count = LARGE_INTEGER();		//	1초에 셀수있는 count
-	LARGE_INTEGER PrevTime = LARGE_INTEGER();	
-	LARGE_INTEGER CurTime = LARGE_INTEGER();
-	
-	double TimeCounter = 0.0f;
-	double dDeltaTime = 0.0;
-	float DeltaTime = 0.0f;
-	
+	LARGE_INTEGER Count;	//	1초에 셀수있는 count
+	LARGE_INTEGER PrevTime;
+	LARGE_INTEGER CurTime;
 
-	
+	double TimeCounter;
+	double dDeltaTime;
+	float DeltaTime;
 
-	
+
+
+
 
 };
 
