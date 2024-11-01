ed31a5cd315b8931af46635f05d97078879c7917
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-11-01 16:55:30 +0900

    흠흠

--- Window_Game/TestProject/BasicAlgo.cpp
+++ Window_Game/TestProject/BasicAlgo.cpp
@@ -14,7 +14,7 @@ void UBasicAlgo::SliceProcess(Dungeon& _Dungeon)
 
 
 	FVector2D SlicedRoomSize = UseableTrans.Scale.Half();
-	int a = 0;
+
 
 	for (int _y = 0; _y < 2; _y++)
 	{
@@ -23,17 +23,29 @@ void UBasicAlgo::SliceProcess(Dungeon& _Dungeon)
 			FVector2D Pos = FVector2D(UseableTrans.Location.iX() + SlicedRoomSize.iX() * _x, UseableTrans.Location.iY() + SlicedRoomSize.iY() * _y);
 			FTransform SlicedRoomTrans = FTransform(SlicedRoomSize, Pos);
 			_Dungeon.SlicedRooms.push_back(SlicedRoomTrans);
-
 		}
 	}
 
 
 
-	int a1 = 0;
+
 }
 
 void UBasicAlgo::RoomGenProcess(Dungeon& _Dungeon)
 {
+	int GenableRoomCount = _Dungeon.SlicedRooms.size();
+
+	int roomCount = 0;
+
+	for (int i = 0; i < GenableRoomCount; i++)
+	{
+		
+		
+		// ·£´ý¹æ»ý¼º
+
+	}
+
+	int a = 0;
 
 }
 
