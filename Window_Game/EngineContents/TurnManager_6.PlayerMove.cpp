0e2b93c1bb4ad1287bdc2bf3f4ad35a0febe3b04
rlatjdgnsdl2 <rlatjdgnsdl2@gmail.com>
2024-11-15 14:43:10 +0900

    이제 진짜 데이터 클래스 구상하기

--- Window_Game/EngineContents/TurnManager_6.PlayerMove.cpp
+++ Window_Game/EngineContents/TurnManager_6.PlayerMove.cpp
@@ -15,12 +15,8 @@ void ATurnManager::PlayerMove(float _DeltaTime)
 {
 	//	Player move
 	Player->Move(_DeltaTime);
-	std::vector<APokemon*>::iterator StartIter = MovePokemon.begin();
-	std::vector<APokemon*>::iterator EndIter = MovePokemon.end();
-	//	 플레이어가 이동할때 MovePokemon에 있는 포켓몬 모두 이동함
-	for (; StartIter != EndIter; StartIter++)
-	{
-		APokemon* CurPokemon = *StartIter;
+	// 플레이어가 이동할때 MovePokemon에 있는 포켓몬 모두 동시에 이동함
+	for (APokemon* CurPokemon : MovePokemon) {
 		CurPokemon->Move(_DeltaTime);
 	}
 	// 플레이어가 도착했으면
@@ -28,18 +24,13 @@ void ATurnManager::PlayerMove(float _DeltaTime)
 	if (CurPlayerLocation == Player->GetTargetLocation())
 	{
 		//	MovePokemon에 있는 포켓몬 모두 강제로 도착하게
-		std::vector<APokemon*>::iterator StartIter = MovePokemon.begin();
-		std::vector<APokemon*>::iterator EndIter = MovePokemon.end();
-		for (; StartIter != EndIter; StartIter++)
-		{
-			APokemon* CurPokemon = *StartIter;
+		for (APokemon* CurPokemon : MovePokemon) {
 			CurPokemon->Move(1.0f);
-			CurPokemon->ResetCurDuration();
 		}
 		//	다 이동했으면 MovePokemon초기화
 		MovePokemon.clear();
-		//	다음단계 - 어짜피 MovePokmon 초기화되서 의미없지만 이동 - 공격이라는 우선순위를 규칙으로 해서 AI_Move로 가게함
-		CurTurnType = TurnType::AI_Move;
+
+		CurTurnType = TurnType::AI_Skill;
 		return;
 	}
 }
